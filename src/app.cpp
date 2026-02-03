426744a75c33e24d4691a3f16d0b58c0327c769b
Alina Dovhan <101360006+alinadovgan@users.noreply.github.com>
2026-02-03 18:35:21 +0000

    Lab 1 Submission

--- src/app.cpp
+++ src/app.cpp
@@ -6,13 +6,48 @@ using namespace std;
 
 bool isPrime(int x);
 int closestPrime(int x);
-/** You should put in a main for testing, but then comment it out for submission.
- * The test.cpp file will include the main in your submission. You do not edit the test.cpp 
- * file. The only file to edit is app.cpp**/
-/**** 
-int main()
-{
-    
-    return 0;          
+
+/*int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (isPrime(a)) {
+        cout << a << " is a prime number.";
+    }
+    else {
+        cout << a << " is not a prime number. Closest prime number is: " << closestPrime(a) << ".\n";
+    }
+
+    if (isPrime(b)) {
+        cout << b << " is a prime number.";
+    }
+    else {
+        cout << b << " is not a prme number. Closest prime number is: " << closestPrime(b) << ".\n";
+    }
+    return 0;
 }
-****/
+    */
+
+bool isPrime(int x) {
+    if(x<=1) 
+    return false;
+
+    for (int i = 2; i * i <= x; i++) {
+        if (x%i == 0)
+        return false;
+    }
+    return true;
+}
+
+int closestPrime(int x) {
+    if (isPrime(x)) 
+    return x;
+
+    int next = x+1;
+    while (!isPrime(next)) {
+        next++;
+    }
+    return next;
+}
